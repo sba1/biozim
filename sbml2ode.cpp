046e01738f26705d8e04848478f25673aa2e97cc
Sebastian Bauer <mail@sebastianbauer.info>
2009-01-29 12:42:22 +0000

    Added support for gnuplot.

--- sbml2ode.cpp
+++ sbml2ode.cpp
@@ -4,6 +4,7 @@
 
 /* Own headers */
 #include "simulation.h"
+#include "gnuplot_i.h"
 
 /********************************************************/
 
@@ -15,6 +16,8 @@ static int output_time;
 static double maxtime;
 static double error;
 static int sample_steps;
+static int plot;
+static char **plot_species;
 
 int verbose; /* used by other modules */
 
@@ -32,6 +35,8 @@ static void usage(char *name)
 			"\t    --force-interpreted  forces the interpreted calculation of the rhs.\n"
 			"\t    --maxtime            specifies the end time (defaults to 1).\n"
 			"\t    --output-time        outputs the current time (stderr).\n"
+			"\t    --plot [sp1,...,spn] plots the results using gnuplot. Optionally, you can specify\n"
+			"\t                         the species to be plotted (defaults to all)\n"
 			"\t    --sample-steps       the number of sample steps (defaults to 5000).\n"
 			"\t    --stiff              use solver for stiff ODEs.\n"
 			"\t    --stochastic         apply stochastic simulation.\n"
@@ -42,9 +47,79 @@ static void usage(char *name)
 }
 
 
-/*************************************************
- Parse command line args
-*************************************************/
+/**
+ * Splits the given character sequence.
+ *
+ * @param str
+ * @param c
+ * @return
+ */
+static char **strsplit(char *str, char c)
+{
+	int count, pos;
+	char **array;
+	char *dup;
+
+	if (!(dup = strdup(str)))
+		return NULL;
+
+	pos = count = 0;
+	while (dup[pos])
+	{
+		if (dup[pos] == c)
+			count++;
+		pos++;
+	}
+
+	if (!(array = (char**)malloc(sizeof(char*) * (count + 2))))
+		return NULL;
+
+	array[0] = dup;
+	array[count+1] = NULL;
+
+	pos = count = 0;
+	while (dup[pos])
+	{
+		if (dup[pos] == c)
+		{
+			array[++count] = &dup[pos+1];
+			dup[pos] = 0;
+		}
+		pos++;
+	}
+
+	return array;
+}
+
+/**
+ * Returns the index of the given string (or -1 if string
+ * couldn't be found) in the given NULL-terminated array.
+ *
+ * @param array
+ * @param str
+ * @return
+ */
+static int strindex(const char **array, const char *str)
+{
+	int i = 0;
+
+	while (array[i])
+	{
+		if (!strcmp(array[i],str))
+			return i;
+		i++;
+	}
+	return -1;
+}
+
+/**
+ * Parses the command line.
+ *
+ * TODO: When increasing i, check for out of bounds error.
+ *
+ * @param argc
+ * @param argv
+ */
 static void parse_args(int argc, char *argv[])
 {
 	int i;
@@ -66,6 +141,22 @@ static void parse_args(int argc, char *argv[])
 		} else if (!strcmp(argv[i],"--force-interpreted"))
 		{
 			force_interpreted = 1;
+		} else if (!strcmp(argv[i],"--plot"))
+		{
+			plot = 1;
+			char *nr_arg;
+
+			if (argv[i][6]=='=')
+				nr_arg = &argv[i][7];
+			else
+			{
+				nr_arg = argv[i+1];
+				if (*nr_arg == '-')
+					continue;
+				i++;
+			}
+			if (!(plot_species = strsplit(nr_arg,',')))
+				fprintf(stderr,"Could not determine the name of species to be plotted!");
 		} else if (!strcmp(argv[i],"--output-time"))
 		{
 			output_time = 1;
@@ -79,8 +170,8 @@ static void parse_args(int argc, char *argv[])
 		{
 			char *nr_arg;
 
-			if (argv[i][9]=='=')
-				nr_arg = &argv[i][10];
+			if (argv[i][14]=='=')
+				nr_arg = &argv[i][15];
 			else
 			{
 				nr_arg = argv[i+1];
@@ -115,8 +206,8 @@ static void parse_args(int argc, char *argv[])
 		{
 			char *nr_arg;
 
-			if (argv[i][9]=='=')
-				nr_arg = &argv[i][10];
+			if (argv[i][7]=='=')
+				nr_arg = &argv[i][8];
 			else
 			{
 				nr_arg = argv[i+1];
@@ -130,12 +221,18 @@ static void parse_args(int argc, char *argv[])
 			}
 			error = strtod(nr_arg, NULL);
 		}
-
 		else
 		{
-			filename_given = 1;
 			if (strcmp(argv[i],"-"))
+			{
 				model_filename = argv[i];
+				filename_given = 1;
+			} else
+			{
+				fprintf(stderr,"Unknown parameter \"%s\"\n",argv[i]);
+				usage(argv[0]);
+				exit(-1);
+			}
 		}
 	}
 
@@ -149,6 +246,24 @@ static void parse_args(int argc, char *argv[])
 	if (sample_steps < 1) sample_steps = 1;
 }
 
+/**
+ * A single sample
+ */
+struct sample
+{
+	struct sample *next;
+
+	double time;
+	int num_values;
+	double *values;
+	int num_strings;
+	char **strings;
+};
+
+static struct sample *samples_first;
+static struct sample *samples_last;
+static int samples_total;
+
 /**********************************************************
  Our sampling function.
 ***********************************************************/
@@ -157,13 +272,39 @@ int sample(double time, int num_values, double *values)
 	if (output_time)
 		fprintf(stderr,"%g\n",time);
 
+	/* Console output */
 	printf("%g",time);
-
 	for (int i=0;i<num_values;i++)
-	{
 		printf("\t%.12g",values[i]);
+
+	if (plot)
+	{
+		struct sample *current = (struct sample*)malloc(sizeof(*current));
+		if (current)
+		{
+			/* Prepare sample */
+			memset(current,0,sizeof(*current));
+			if (!(current->values = (double*)malloc(num_values*sizeof(double))))
+			{
+				free(current);
+				goto leave;
+			}
+
+			current->num_values = num_values;
+			for (int i=0;i<num_values;i++)
+				current->values[i] = values[i];
+			current->time = time;
+
+			/* Enqueue sample */
+			if (!samples_first) samples_first = current;
+			if (samples_last)
+				samples_last->next = current;
+			samples_last = current;
+			samples_total++;
+		}
 	}
 
+leave:
 	return 1;
 }
 
@@ -182,9 +323,36 @@ int sample_strings(double time, int num_strings, char **values)
 	return 1;
 }
 
-/**********************************************************
- Main Entry
-***********************************************************/
+/**
+ * Plots the given value.
+ *
+ * @param ctrl
+ * @param which
+ * @return
+ */
+void plot_samples(gnuplot_ctrl *ctrl, int which, const char *name, double *x, double *y)
+{
+	struct sample *s = samples_first;
+	int i = 0;
+
+	while (s)
+	{
+		x[i] = s->time;
+		y[i] = s->values[which];
+
+		i++;
+		s = s->next;
+	}
+	gnuplot_plot_xy(ctrl,x,y,samples_total,name);
+}
+
+/**
+ * Main entry
+ *
+ * @param argc
+ * @param argv
+ * @return
+ */
 int main(int argc, char **argv)
 {
 	struct simulation_context *sc;
@@ -220,6 +388,45 @@ int main(int argc, char **argv)
 	settings.stiff = stiff;
 
 	simulation_integrate(sc,&settings);
+
+	if (plot && samples_first)
+	{
+		double *x, *y;
+		gnuplot_ctrl *ctrl;
+
+		if (!(x = (double*)malloc(samples_total*sizeof(double))))
+			goto bailout;
+		if (!(y = (double*)malloc(samples_total*sizeof(double))))
+			goto bailout;
+
+		if (!(ctrl = gnuplot_init()))
+		{
+			fprintf(stderr,"Couldn't initialize gnuplot!");
+			goto bailout;
+		}
+
+		if (plot_species)
+		{
+			for (int j=0;plot_species[j];j++)
+			{
+				int idx = strindex(names,plot_species[j]);
+				if (idx == -1)
+				{
+					fprintf(stderr,"Species \"%s\" couldn't not be found!\n",plot_species[j]);
+					continue;
+				}
+				plot_samples(ctrl,idx,names[idx],x,y);
+			}
+		} else
+		{
+			for (int idx=0;idx<samples_first->num_values;idx++)
+				plot_samples(ctrl,idx,names[idx],x,y);
+		}
+
+		fprintf(stderr,"Press Enter to quit!\n");
+		while (getchar()!='\n') {}
+		gnuplot_close(ctrl);
+	}
 
 	simulation_context_free(sc);
 
